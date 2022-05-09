19298c53eadd137dfbfa84a30419874e27571239
Justin Morrow <morrowjustin19@gmail.com>
2022-05-08 18:18:38 -0700

    updating strucutre

--- script.js
+++ script.js
@@ -1,56 +1,42 @@
   var time = moment().format("hh:mm:ss");
   $("#time").text(time);
-  var today = moment();
-  $("#day").text(today.format("MMM Do, YYYY"));
-  
-  var timeBlock = $("#time-block");
-  var armytime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-  for (var i = 0; i<armytime.length; i++) {
-    var thisHour = parseInt(moment().format("H"));
-    console.log( armytime[i] < thisHour )
-    $("#"+armytime[i]).text(JSON.parse(localStorage.getItem( armytime[i] )))
-    if(armytime[i] < thisHour){
-      $("#"+armytime[i]).addClass("past")
-    }else if (thisHour === armytime[i]){
-      $("#"+armytime[i]).removeClass("past")
-      $("#"+armytime[i]).addClass("present")
-    } else {
-      $("#"+armytime[i]).removeClass("past")
-      $("#"+armytime[i]).removeClass("present")
-      $("#"+armytime[i]).addClass("future")
-    }
+var today = moment();
+$("#day").text(today.format("MMM Do, YYYY"));
+var sheduleFormEl = $("#shedule-form");
+var toDoListEl = $("#to-do-list");
+var saveEl = $("#icon")
+var timeBlock = $(".time-block")
+var armytime = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+for (var i = 0; i<armytime.length; i++) {
+  var thisHour = parseInt(moment().format("H"));
+  console.log( armytime[i] < thisHour )
+  $("#"+armytime[i]).text(JSON.parse(localStorage.getItem( armytime[i] )))
+  if(armytime[i] < thisHour){
+    $("#"+armytime[i]).addClass("past")
+  }else if (thisHour === armytime[i]){
+    $("#"+armytime[i]).removeClass("past")
+    $("#"+armytime[i]).addClass("present")
+  } else {
+    $("#"+armytime[i]).removeClass("past")
+    $("#"+armytime[i]).removeClass("present")
+    $("#"+armytime[i]).addClass("future")
   }
-
-  // The function that makes it so the text stays in the schedule when button clicked
-  function handleSubmitEvent(e) {
-      console.log("adding click event")
-      // prevent default method 
-      e.preventDefault();
-  
-      // define variables 1) Define a variable that is an activity and 1 that shows me when an activty has to be done
-      // var activity = $(this).siblings('.activity-text').val();
-      console.log($(this));
-      var activity = $(this).prev().val().trim();
-      var id = $(this).prev().attr("id");
-      // var activity = id.val();
-      console.log("my activity ", activity); 
-      console.log("my id ", id); 
-      // If they don't type anything in certain hour what will happen?
-      if(!activity){
-          console.log("the user didn't enter an activity");
-          return;
-      }
-      // What will happen if they do type something in a certain hour?
-      // else {
-          localStorage.setItem(id, JSON.stringify(activity));
-          // localStorage.getItem("activity")
-  
-      // }
-  
-  
-  
-  }
-  
-  // Create a submit event listener on the form element
-  timeBlock.on("click", ".savebutton", handleSubmitEvent);
-  
+$("#"+armytime[i]).append()
+}
+// create function to handle form submission
+function handleFormSubmit(event) {
+  event.preventDefault();
+  console.log($(this).prev().val());
+  // select form element by its `name` attribute and get its value
+  var activity = $(this).prev().val().trim();
+  var id = $(this).prev().attr("id")
+  // if there's nothing in the form entered, don't print to the page
+  if (!activity) {
+    console.log('No things to do at this moment!');
+    return;
+  };
+      localStorage.setItem(id, JSON.stringify(activity));
+     // localStorage.getItem("activity")
+};
+// Create a submit event listener on the form element
+timeBlock.on('click', ".saveBtn", handleFormSubmit);
